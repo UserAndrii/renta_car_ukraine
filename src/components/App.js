d9aed6af516fd5e41164381a7785da911a4daad4
Andrii Gadar <gadar.andre@gmail.com>
2023-09-18 12:17:53 +0300

    Project settings, added GitHub Actions for automatic build

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        margin: "0 auto",
+        width: 1440,
+      }}
+    ></div>
+  );
+}
+
+export default App;
